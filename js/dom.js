fdccf321c47aae0e6a771a57ad0c145e4ae5ebfb
Maskur Ahmed <maskurahmed37@gmail.com>
2025-08-22 23:49:04 +0600

    Update innerHTML of the element with ID 'fruit' in dom.js

--- js/dom.js
+++ js/dom.js
@@ -4,6 +4,7 @@ let cls = document.getElementsByClassName("someclass");
 console.log(cls);
 
 let fru = document.querySelector('#fruit');
+fru.innerHTML = "maskur's fruit"
 console.log(fru);
 
 let fc = document.getElementsByClassName("f_collection");
