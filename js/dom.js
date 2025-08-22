aab7933ca24bce429e854b1f25173ba7231c94a8
Maskur Ahmed <maskurahmed37@gmail.com>
2025-08-22 23:39:48 +0600

    Fix formatting issue in index.html and simplify logging in dom.js

--- js/dom.js
+++ js/dom.js
@@ -1,15 +1,2 @@
-let lists = document.getElementsByTagName("h1");
-console.log(lists);
-let place = document.getElementById("place");
-console.log(place);
-console.log(place.innerText);
-let qu = document.querySelector('#fruit');
-console.log(qu);
-let qAll = document.querySelectorAll(".f_collection")
-
-for( const li of qAll){
-    console.log(li.innerText)
-}
-
-let first = document.getElementById("first");
-console.log(first.classList)
+let h1 = document.getElementById('first');
+console.log(h1);
