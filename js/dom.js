0a807978dfff80ea9afaed452dc7aba4ec59d1c0
Maskur Ahmed <maskurahmed37@gmail.com>
2025-08-24 00:41:16 +0600

    Uncomment main section in index.html and add event listener to change background color in event.js

--- js/dom.js
+++ js/dom.js
@@ -1,82 +1,82 @@
+// // let h1 = document.getElementById('first');
+// // h1.innerHTML = "khawa dawa";
+// // // h1.classList.remove("someclass");
+// // console.log(h1)
+// // console.log(h1.classList);
+// // let cls = document.getElementsByClassName("someclass");
+// // console.log(cls);
+
+// // let fru = document.querySelector('#fruit');
+// // fru.innerHTML = "maskur's fruit"
+// // console.log(fru.innerHTML);
+
+// // let fc = document.getElementsByClassName("f_collection");
+// // console.log(fc);
+
+// // let plc = document.querySelectorAll('.place');
+// // console.log(plc);
+
+// // // h1.setAttribute('class','democlass');
+
+// // localStorage.setItem('username','Alex');
+// // let rem = localStorage.getItem('username');
+// // console.log(rem);
+// // localStorage.removeItem('username');
+
+// // let sections = document.querySelectorAll('section');
+
+// // for(const sec of sections){
+// //     sec.style.backgroundColor="green"
+// //     sec.style.border = '2px solid blue'
+// // }
+// //========================================================================;
+// let main = document.getElementById("main");
 // let h1 = document.getElementById('first');
-// h1.innerHTML = "khawa dawa";
-// // h1.classList.remove("someclass");
-// console.log(h1)
-// console.log(h1.classList);
-// let cls = document.getElementsByClassName("someclass");
-// console.log(cls);
+// h1.innerHTML = 'vober bati'
+// let section = document.createElement('section');
+// section.innerHTML=`
+// <h1>what i learned</h1>
+// <ul>
+//    <li>html</li>
+//    <li>css</li>
+//    <li>javascript</li>
+//    <li>tailwind</li>
+// </ul>
+// `;
 
-// let fru = document.querySelector('#fruit');
-// fru.innerHTML = "maskur's fruit"
-// console.log(fru.innerHTML);
+// main.appendChild(section);
 
-// let fc = document.getElementsByClassName("f_collection");
-// console.log(fc);
+// let fList = document.getElementById("first_list");
 
-// let plc = document.querySelectorAll('.place');
-// console.log(plc);
+// let li = document.createElement("li");
+// li.innerText = "lokochoko";
 
-// // h1.setAttribute('class','democlass');
-
-// localStorage.setItem('username','Alex');
-// let rem = localStorage.getItem('username');
-// console.log(rem);
-// localStorage.removeItem('username');
+// fList.appendChild(li);
 
 // let sections = document.querySelectorAll('section');
-
-// for(const sec of sections){
-//     sec.style.backgroundColor="green"
-//     sec.style.border = '2px solid blue'
+// for(const section of sections){
+//     section.style.backgroundColor="purple";
+//     section.style.padding='5px';
+//     section.style.marginBottom='10px';
+//     section.style.color='white';
+//     section.style.borderRadius='20px';
 // }
-//========================================================================;
-let main = document.getElementById("main");
-let h1 = document.getElementById('first');
-h1.innerHTML = 'vober bati'
-let section = document.createElement('section');
-section.innerHTML=`
-<h1>what i learned</h1>
-<ul>
-   <li>html</li>
-   <li>css</li>
-   <li>javascript</li>
-   <li>tailwind</li>
-</ul>
-`;
 
-main.appendChild(section);
-
-let fList = document.getElementById("first_list");
-
-let li = document.createElement("li");
-li.innerText = "lokochoko";
-
-fList.appendChild(li);
-
-let sections = document.querySelectorAll('section');
-for(const section of sections){
-    section.style.backgroundColor="purple";
-    section.style.padding='5px';
-    section.style.marginBottom='10px';
-    section.style.color='white';
-    section.style.borderRadius='20px';
-}
-
-let tList = document.querySelector("#third_list");
+// let tList = document.querySelector("#third_list");
 
 
-let li2 = document.createElement("li");
+// let li2 = document.createElement("li");
 
-li2.innerText='reactjs';
+// li2.innerText='reactjs';
 
-tList.appendChild(li2);
+// tList.appendChild(li2);
 
-let cNode = tList.childNodes[3].nextSibling.nextSibling.innerText;
-console.log(cNode)
+// let cNode = tList.childNodes[3].nextSibling.nextSibling.innerText;
+// console.log(cNode)
 
-let places = document.getElementsByClassName('place');
+// let places = document.getElementsByClassName('place');
 
-for(const place of places){
-    place.style.backgroundColor="gray";
-    place.style.padding='5px';
-}
+// for(const place of places){
+//     place.style.backgroundColor="gray";
+//     place.style.padding='5px';
+// }
