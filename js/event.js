8172c464f4a6774c288e42f9c6a80d19f56fdb48
Maskur Ahmed <maskurahmed37@gmail.com>
2025-08-24 00:49:58 +0600

    Add IDs to buttons in index.html and define makeBlue function in event.js

--- js/event.js
+++ js/event.js
@@ -1,4 +1,18 @@
+////////////////////////////////////////////////
+//*option 2:-
+function makeBlue(){
+    document.body.style.backgroundColor="green"
+}
+
 let btn1 = document.getElementById('btn1');
 btn1.addEventListener('click',()=>{
     document.body.style.backgroundColor='red';
 });
+
+let firstBtn = document.getElementById('first');
+firstBtn.onclick =makePurple;
+
+function makePurple(){
+    document.body.style.backgroundColor="purple";
+
+}
