6ef7dc7ae3ae30c2a112388486bb82f362fbdb8f
Maskur Ahmed <maskurahmed37@gmail.com>
2025-08-23 22:56:38 +0600

    Refactor CSS and update JavaScript to create new section and append list item

--- js/dom.js
+++ js/dom.js
@@ -1,31 +1,54 @@
+// let h1 = document.getElementById('first');
+// h1.innerHTML = "khawa dawa";
+// // h1.classList.remove("someclass");
+// console.log(h1)
+// console.log(h1.classList);
+// let cls = document.getElementsByClassName("someclass");
+// console.log(cls);
+
+// let fru = document.querySelector('#fruit');
+// fru.innerHTML = "maskur's fruit"
+// console.log(fru.innerHTML);
+
+// let fc = document.getElementsByClassName("f_collection");
+// console.log(fc);
+
+// let plc = document.querySelectorAll('.place');
+// console.log(plc);
+
+// // h1.setAttribute('class','democlass');
+
+// localStorage.setItem('username','Alex');
+// let rem = localStorage.getItem('username');
+// console.log(rem);
+// localStorage.removeItem('username');
+
+// let sections = document.querySelectorAll('section');
+
+// for(const sec of sections){
+//     sec.style.backgroundColor="green"
+//     sec.style.border = '2px solid blue'
+// }
+//========================================================================;
+let main = document.getElementById("main");
 let h1 = document.getElementById('first');
-h1.innerHTML = "khawa dawa";
-// h1.classList.remove("someclass");
-console.log(h1)
-console.log(h1.classList);
-let cls = document.getElementsByClassName("someclass");
-console.log(cls);
+h1.innerHTML = 'vober bati'
+let section = document.createElement('section');
+section.innerHTML=`
+<h1>what i learned</h1>
+<ul>
+   <li>html</li>
+   <li>css</li>
+   <li>javascript</li>
+   <li>tailwind</li>
+</ul>
+`;
 
-let fru = document.querySelector('#fruit');
-fru.innerHTML = "maskur's fruit"
-console.log(fru.innerHTML);
+main.appendChild(section);
 
-let fc = document.getElementsByClassName("f_collection");
-console.log(fc);
+let fList = document.getElementById("first_list");
 
-let plc = document.querySelectorAll('.place');
-console.log(plc);
+let li = document.createElement("li");
+li.innerText = "lokochoko";
 
-// h1.setAttribute('class','democlass');
-
-localStorage.setItem('username','Alex');
-let rem = localStorage.getItem('username');
-console.log(rem);
-localStorage.removeItem('username');
-
-let sections = document.querySelectorAll('section');
-
-for(const sec of sections){
-    sec.style.backgroundColor="green"
-    sec.style.border = '2px solid blue'
-}
+fList.appendChild(li);
