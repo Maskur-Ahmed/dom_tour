57b71347f1ab4a2adef187f4060db4db335a6728
Maskur Ahmed <maskurahmed37@gmail.com>
2025-08-24 11:18:33 +0600

    option 4

--- js/event.js
+++ js/event.js
@@ -18,13 +18,22 @@ function makePurple(){
     document.body.style.backgroundColor="purple";
 
 }
+//=============================================================;
+ //*option:2
+
 function makeYollow(){
     document.body.style.backgroundColor="yellow";
 }
-
+//*option:3
 let black = document.getElementById('black');
 black.onclick=makeBlack;
 
 function makeBlack(){
     document.body.style.backgroundColor='black';
 }
+//===========================================================
+//* option:4
+let btnP = document.getElementById('btnP');
+btnP.addEventListener('click',()=>{
+    document.body.style.backgroundColor='pink'
+})
