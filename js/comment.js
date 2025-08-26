adf0fafcc9f8217ca74ed67c8fe032469fff8069
Maskur Ahmed <maskurahmed37@gmail.com>
2025-08-26 23:49:17 +0600

    i get the comment from comment box

--- js/comment.js
+++ js/comment.js
@@ -3,5 +3,5 @@ document.getElementById('postBtn').addEventListener('click',()=>{
     // get the comment inside the textarea;
     let commentBox = document.getElementById('commentArea');
     let commentValue = commentBox.value;
-    console.log(commentValue);
+    
 })
