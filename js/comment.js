e1f8a35b0b81180794688bdb84a0dc7da1ef6ab8
Maskur Ahmed <maskurahmed37@gmail.com>
2025-08-26 23:52:23 +0600

    i get the comment container

--- js/comment.js
+++ js/comment.js
@@ -3,5 +3,8 @@ document.getElementById('postBtn').addEventListener('click',()=>{
     // get the comment inside the textarea;
     let commentBox = document.getElementById('commentArea');
     let commentValue = commentBox.value;
+    // push the comment into the main area;
+    let commentContainer = document.getElementById('commentContainer');
     
+
 })
