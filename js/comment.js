846da200cee56fc47c56b1fcf97dda46477d0313
Maskur Ahmed <maskurahmed37@gmail.com>
2025-08-26 23:48:52 +0600

    i get the button

---
+++ js/comment.js
@@ -0,0 +1,7 @@
+// step:1 get the button;
+document.getElementById('postBtn').addEventListener('click',()=>{
+    // get the comment inside the textarea;
+    let commentBox = document.getElementById('commentArea');
+    let commentValue = commentBox.value;
+    console.log(commentValue);
+})
