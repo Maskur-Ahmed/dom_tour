b98f473e4e8e116b541fd0e4be587ad923d63672
Maskur Ahmed <maskurahmed37@gmail.com>
2025-08-30 23:15:34 +0600

    five

--- js/comment.js
+++ js/comment.js
@@ -15,6 +15,7 @@ document.getElementById('postBtn').addEventListener('click',()=>{
 });
 
 function del(){
+    
  document.querySelectorAll('.coment');
  let delBtn = document.getElementById('del');
  delBtn.style='display:none'
