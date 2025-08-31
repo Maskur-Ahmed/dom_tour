e83e6409f668c81d59a2e4ad2ca7a4d817cba637
Maskur Ahmed <maskurahmed37@gmail.com>
2025-08-31 23:19:47 +0600

    apply comment delete style

--- js/comment.js
+++ js/comment.js
@@ -16,7 +16,9 @@ document.getElementById('postBtn').addEventListener('click',()=>{
 
 function del(){
     // select the comment area;
- document.querySelectorAll('.coment');
+  let comment = document.querySelectorAll('.coment');
+  //comment deleted
+  comment.style='display:none'
  //select the delete button;
  let delBtn = document.getElementById('del');
  //apply the style
