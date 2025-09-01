6480ac84c106dee82bb045b94054c3351bd7e0ff
Maskur Ahmed <maskurahmed37@gmail.com>
2025-09-01 15:19:34 +0600

    append child added

--- js/stylilng.js
+++ js/stylilng.js
@@ -1,3 +1,8 @@
 let pS = document.getElementById('placesSection');
 
 console.log(pS.parentNode.childNodes[1].nextSibling);
+let li = document.createElement('li');
+
+li.innerText='hi am i looking good';
+
+pS.appendChild(li);
