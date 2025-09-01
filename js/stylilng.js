2cfdef62305c8a549eb79e1b1aa6727c9970033f
Maskur Ahmed <maskurahmed37@gmail.com>
2025-09-01 15:28:12 +0600

    section created

--- js/stylilng.js
+++ js/stylilng.js
@@ -6,3 +6,12 @@ let li = document.createElement('li');
 li.innerText='hi am i looking good';
 
 pS.appendChild(li);
+let main = document.getElementById('main');
+let sec = document.createElement('section');
+let h1 = document.createElement('h1');
+h1.innerText ='foods i like';
+let p = document.createElement('p');
+p.innerText = 'the foods are very good';
+sec.appendChild(h1);
+sec.appendChild(p);
+main.appendChild(sec);
